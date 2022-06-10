78b58fffa46e5eee99f27da80e05a508f72cb1a3
madlabman <madlabman@mail.ru>
2022-06-10 10:14:15 +0300

    feat(metrics): add build info metric

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from prometheus_client import start_http_server
 
 from bot import AAVEBot
 from bot.config import EXPORTER_PORT
+from bot.metrics import report_build_info
 
 log = logging.getLogger(__name__)
 
@@ -13,5 +14,6 @@ log = logging.getLogger(__name__)
 if __name__ == "__main__":
     log.info("Starting prometheus exporter server on port %s", EXPORTER_PORT)
     start_http_server(EXPORTER_PORT)
+    report_build_info()
     bot = AAVEBot()
     bot.run()
