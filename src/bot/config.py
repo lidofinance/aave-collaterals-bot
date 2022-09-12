0b96ce2adfc26a8feaaf41ee24dd334578014547
madlabman <10616301+madlabman@users.noreply.github.com>
2022-09-12 19:37:36 +0200

    fix: passthrough log level

--- src/bot/config.py
+++ src/bot/config.py
@@ -12,8 +12,22 @@ if os.getenv("LOG_FORMAT", "simple") == "json":
     formatter = jsonlogger.JsonFormatter("%(asctime)%(levelname)%(name)%(message)")
     logging_handler.setFormatter(formatter)
 
-logging.basicConfig(level=logging.INFO, handlers=(logging_handler,))
+LOG_LEVEL_DEFAULT = logging.INFO
+LOG_LEVEL = os.getenv("LOG_LEVEL", logging.getLevelName(LOG_LEVEL_DEFAULT))
+LOG_LEVEL_IS_VALID = getattr(logging, LOG_LEVEL, None) is not None
+
+logging.basicConfig(
+    level=LOG_LEVEL if LOG_LEVEL_IS_VALID else LOG_LEVEL_DEFAULT,
+    handlers=(logging_handler,),
+)
 log = logging.getLogger(__name__)
+
+if not LOG_LEVEL_IS_VALID:
+    log.warning(
+        "%s was not recognized, defaults to %s",
+        f"{LOG_LEVEL=}",
+        logging.getLevelName(LOG_LEVEL_DEFAULT),
+    )
 
 T: TypeAlias = Any
 D = object()  # sentinel
